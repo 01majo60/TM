e0a3d15371ffe97f215866b86e75b1a389625264
01majo60 <37268373+01majo60@users.noreply.github.com>
2018-05-24 22:24:52 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,17 @@ from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+
+def create_app(config_class=Config):
+  app = Flask(__name__)
+  app.config.from_object(Config)
+  db.init_app(app)
+  migrate.init_app(app, db)
+  bootstrap.init_app(app)
+  
+  return app
 
 from app import routes, models,errors
